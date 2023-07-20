5efb2efca2da3cc697f5875140d99c26dde853ad
George Bordukov <george.bordukov@gmail.com>
2023-07-20 17:07:40 +0300

    update

--- src/test/java/org/example/GetPeopleInfoTest.java
+++ src/test/java/org/example/GetPeopleInfoTest.java
@@ -5,12 +5,10 @@ import io.restassured.response.ValidatableResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
-import java.util.Collections;
 import static testData.Variables.*;
 
 @Slf4j
@@ -28,9 +26,11 @@ public class GetPeopleInfoTest {
 
     @Test
     public void CheckSortingByPriceAsc () {
-        String URL = "https://kcentr.ru/content-service/api/desktop/v1/products?visitorUuid=8485f7ea-f2e7-4478-aff3-e8fe3b0f3cf0&cityUuid=deb1d05a-71ce-40d1-b726-6ba85d70d58f&categoryId=televizory&sortType=price_asc&limit=19";
+        String URL = "https://kcentr.ru/content-service/api/desktop/v1/products";
         log.info("Performing GET request with URL: " + URL);
-        ValidatableResponse valResponse = given().when().get(URL).then()
+        ValidatableResponse valResponse = given()
+                .queryParams(queryParameters)
+                .when().get(URL).then()
                 .assertThat()
                 .statusCode(200)
                 .body("products.mainCharacteristics[2].name[0]", equalTo("Диагональ"));
@@ -40,13 +40,53 @@ public class GetPeopleInfoTest {
         Collections.sort(pricesExpectedSorting);
         System.out.println(pricesActualSorting);
         System.out.println(pricesExpectedSorting);
+        System.out.println(queryParameters);
         Assert.assertEquals(pricesActualSorting, pricesExpectedSorting);
     }
 
     @Test
-    public void arrayOperations(){
-        int[] array = {15, 46, 8 ,2};
-        for (int i: array)
-        Assert.assertTrue(array[0] > array[3]);
+    public void checkFilterByBrand() {
+
+    }
+
+    @Test
+    public void arrayOperations() {
+        int[] array = {15, 46, 8, 2};
+        for (int i : array) {
+            Assert.assertTrue(array[1] == 46);
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        for (int i : array) {
+            System.out.println(i);
+        }
+        String first = "Jack\fDow";
+        String last = "Dickens";
+        Math.max(25, 6);
+        int time = 11;
+        String result = (time < 18) ? "Good morning" : "Good evening";
+//        switch (14) {
+//            case 12:
+//                System.out.println("No");
+//                break;
+//            case 18:
+//                System.out.println("Yes");
+//                break;
+//            case 13:
+//                System.out.println("NoN");
+//                break;
+//            default:
+//                System.out.println("default");
+//        int x = 0;
+//        while (x > 5) {
+//            System.out.println(x);
+//            x++;
+//        }
+//        do {
+//            System.out.println(x);
+//            x++;
+//        }
+//        while (x > 5);
     }
 }
--- src/test/java/testData/Variables.java
+++ src/test/java/testData/Variables.java
@@ -1,8 +1,16 @@
 package testData;
 
+import java.util.Map;
 public class Variables {
     public static String SCHEMA = "https:";
     public static String BASEHOST = "//swapi.dev";
     public static String PATH = "/api/people/1/";
     public static String SCHEMA_BASEHOST_PATH = SCHEMA + BASEHOST + PATH;
+    public static Map<String, String> queryParameters = Map.of(
+            "visitorUuid", "8485f7ea-f2e7-4478-aff3-e8fe3b0f3cf0",
+            "cityUuid", "deb1d05a-71ce-40d1-b726-6ba85d70d58f",
+            "categoryId", "televizory",
+            "sortType", "price_asc",
+            "limit", "19"
+    );
 }
